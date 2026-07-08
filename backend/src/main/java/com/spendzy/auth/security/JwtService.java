b1495fa96d27e06d07bb7288a6ca1b872d575dbf
AMSIT1 <34167142+AMSIT1@users.noreply.github.com>
2026-07-08 06:38:08 +0530

    Delete backend/src/main/java/com/spendzy/auth/security directory

--- backend/src/main/java/com/spendzy/auth/security/JwtService.java
+++
@@ -1,8 +0,0 @@
-package com.spendzy.auth.security;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class JwtService {
-    // TODO: We'll complete this implementation during Authentication 2A.
-}
