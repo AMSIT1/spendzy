085e658b59c4f9722b4596c00b069389255b541a
AMSIT1 <34167142+AMSIT1@users.noreply.github.com>
2026-07-08 05:40:52 +0530

    Add files via upload

--- backend/src/main/java/com/spendzy/SpendzyApplication.java
+++ backend/src/main/java/com/spendzy/SpendzyApplication.java
@@ -1,1 +1,7 @@
-
+package com.spendzy;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+@SpringBootApplication
+public class SpendzyApplication{
+ public static void main(String[] args){SpringApplication.run(SpendzyApplication.class,args);}
+}
