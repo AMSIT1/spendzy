f0ce851f43e0448b2eac3693fc870698126ddce4
AMSIT1 <34167142+AMSIT1@users.noreply.github.com>
2026-07-08 06:37:34 +0530

    Add files via upload

---
+++ backend/src/main/java/com/spendzy/security/JwtService.java
@@ -0,0 +1,8 @@
+package com.spendzy.auth.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    // TODO: We'll complete this implementation during Authentication 2A.
+}
